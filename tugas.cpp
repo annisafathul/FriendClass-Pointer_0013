ed3836732715dbfc3b29cd30fb28bb66ea1ea700
annisafathul <annisafathul18@gmail.com>
2026-04-27 16:06:35 +0700

    menambahkan fungsi void input data

--- tugas.cpp
+++ tugas.cpp
@@ -10,4 +10,17 @@ private:
     double sisi1, sisi2;
 
 public:
+   void inputData()
+        {
+            cout << "=== Input Layang-Layang ===" << endl;
+            cout << "Masukkan diagonal 1 (d1): ";
+            cin >> d1;
+            cout << "Masukkan diagonal 2 (d2): ";
+            cin >> d2;
+            cout << "Masukkan sisi 1        : ";
+            cin >> sisi1;
+            cout << "Masukkan sisi 2        : ";
+            cin >> sisi2;
+        }
+
     
